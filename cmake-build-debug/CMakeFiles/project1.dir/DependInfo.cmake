25d77daa5b0db0fb72608975628966e8bbabd0fc
Casper Siksma <casper@actinius.com>
2020-04-28 17:14:11 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/project1.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/src/CircularWireConstraint.cpp" "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/cmake-build-debug/CMakeFiles/project1.dir/src/CircularWireConstraint.cpp.o"
+  "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/src/Particle.cpp" "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/cmake-build-debug/CMakeFiles/project1.dir/src/Particle.cpp.o"
+  "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/src/ParticleToy.cpp" "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/cmake-build-debug/CMakeFiles/project1.dir/src/ParticleToy.cpp.o"
+  "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/src/RodConstraint.cpp" "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/cmake-build-debug/CMakeFiles/project1.dir/src/RodConstraint.cpp.o"
+  "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/src/Solver.cpp" "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/cmake-build-debug/CMakeFiles/project1.dir/src/Solver.cpp.o"
+  "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/src/SpringForce.cpp" "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/cmake-build-debug/CMakeFiles/project1.dir/src/SpringForce.cpp.o"
+  "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/src/imageio.cpp" "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/cmake-build-debug/CMakeFiles/project1.dir/src/imageio.cpp.o"
+  "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/src/linearSolver.cpp" "/Users/falker/Documents/University/2IMV15/Assignment/2IMV15-Simulation-in-Computer-Graphics/cmake-build-debug/CMakeFiles/project1.dir/src/linearSolver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/common"
+  "../include/linux"
+  "../include"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/SpringForce.cpp
@@ -0,0 +1,19 @@
+#include "SpringForce.h"
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+SpringForce::SpringForce(Particle *p1, Particle * p2, double dist, double ks, double kd) :
+  m_p1(p1), m_p2(p2), m_dist(dist), m_ks(ks), m_kd(kd) {}
+
+void SpringForce::draw()
+{
+  glBegin( GL_LINES );
+  glColor3f(0.6, 0.7, 0.8);
+  glVertex2f( m_p1->m_Position[0], m_p1->m_Position[1] );
+  glColor3f(0.6, 0.7, 0.8);
+  glVertex2f( m_p2->m_Position[0], m_p2->m_Position[1] );
+  glEnd();
+}
