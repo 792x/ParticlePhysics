d78e95f62ae44671179339e2685db3329982ab96
Bob125 <35141943+Bob125@users.noreply.github.com>
2020-05-09 14:26:12 +0200

    fix for a mistake in the position computation

--- SICGConsole/SICGConsole/solvers/Euler.cpp
+++ SICGConsole/SICGConsole/solvers/Euler.cpp
@@ -7,15 +7,15 @@ void Euler::simulation_step(std::vector<Particle*> pVector, float dt) {
 	
 	// loop through all the particles
 	for (int i = 0; i < int(pVector.size()); i++) {
-		Vec3f oldPosition = pVector[i]->m_Position;
-		Vec3f oldVelocity = pVector[i]->m_Velocity;
-		Vec3f acceleration = pVector[i]->m_Force / pVector[i]->m_Mass;
+		Vec3f initPos = pVector[i]->m_Position;
+		Vec3f initVel = pVector[i]->m_Velocity;
+		Vec3f initAcc = pVector[i]->m_Force / pVector[i]->m_Mass;
 
-		Vec3f newVelocity = oldVelocity + acceleration * dt;
-		Vec3f newPosition = oldPosition + newVelocity * dt;
+		Vec3f finalVelocity = initVel + initAcc * dt;
+		Vec3f finalPosition = initPos + initVel * dt + float(0.5) * initAcc * dt * dt;
 		
-		pVector[i]->m_Velocity = newVelocity;
-		pVector[i]->m_Position = newPosition;
+		pVector[i]->m_Position = finalPosition;
+		pVector[i]->m_Velocity = finalVelocity;
 	}
 
 }
